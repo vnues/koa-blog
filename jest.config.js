7e14d23889306a05ba37d9b9d0aef6e9273d3485
wanggangfeng <wanggangfeng@forchange.tech>
2019-12-04 18:12:20 +0800

    feat: 🎸修改正则

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,10 @@ module.exports = {
   preset: 'ts-jest',
   roots: ['<rootDir>/__tests__/'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx', 'node'],
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  testRegex: '(/__tests__/.*|(.|/)(test|spec)).(jsx?|tsx?)$',
   testEnvironment: 'node',
   // 编译
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+.tsx?$': 'ts-jest'
   }
 }
